d716e5f74d21cb2ccfda6dc4e049f0b535430617
Hayuza <88662028+Hayuza@users.noreply.github.com>
2021-08-09 19:48:38 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Youtube Dl bot
+## Prerequisite
+    ffmpeg
+  
+    
+## install dependencies
+    pip3 install -r requirements.txt
+
+
+## Setup Bot
+    - Change configuration config.py  File
+    - install dependencies
+    - python3 -m bot
+    
+## Thanks
+* [Spechide](https://telegram.dog/SpEcHIDe) for his [AnyDlBot](https://github.com/SpEcHiDe/AnyDLBot)
+* [HasibulKabir](https://telegram.dog/HasibulKabir)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Hayuza/hyzbot/tree/main)
---
+++ plugins/start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Channel", url="https://t.me/HayuzaYT")],
+        [InlineKeyboardButton(
+            "Report Bugs ", url="https://t.me/Hayuza")]
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
